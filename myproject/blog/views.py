5af60fb2f819c80d175692b1bf8d233cd3711e90
kazu744 <oyamakazuma1@gmail.com>
2025-06-12 06:30:14 +0900

    ブログページ完成

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from blog.models import Post
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.all().order_by("-date_posted")
     return render(request, "post_list.html", {"posts": posts})
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, "post_detail.html", {"post": post})
